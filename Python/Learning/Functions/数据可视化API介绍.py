af849369bde2833fbfc951b5ff5a1a63bf55ee7f
Eksi123 <94771689+Eksi123@users.noreply.github.com>
2022-02-13 16:57:44 +0800

    更新了关于API-seaborn的运用

--- Python/Learning/Functions/数据可视化API介绍.py
+++ Python/Learning/Functions/数据可视化API介绍.py
@@ -2,6 +2,7 @@
 
 # 1 基本属性：以最简单的折线图为例
 # (1) 自定义或导入数据
+from tracemalloc import Snapshot
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -87,6 +88,8 @@ axes2.plot(X,Y2)
 plt.show()
 """
 
+#---------------------------------------------------------------#
+
 # 2 基本绘图种类：折线图已介绍
 # (1) 散点图
 import matplotlib.pyplot as plt
@@ -150,4 +153,87 @@ fig=plt.figure()
 plt.pie(Y,autopct="%.2f%%", labels=label) # autopct="%.2f%%"表示各项比率大小按两位小数展示
 plt.show()
 
-# serborn
+# (6) 三维图：包括三维曲线图，曲面图，柱状图和散点图
+import matplotlib.pyplot as plt
+import numpy as np
+
+X=np.array(range(1,11))
+Y=np.random.normal(0,1,size=10)
+Z=np.sin(Y)+np.sqrt(X)
+
+XX,YY=np.meshgrid(X,Y) # 由坐标向量构建坐标矩阵
+ZZ=np.sin(YY)+np.sqrt(XX)
+height=np.zeros_like(Z) # 3D柱状图中柱的底部位置
+width=depth=0.3 # 3D柱状图中柱的长度和宽度
+
+fig=plt.figure()
+axes1=fig.add_subplot(2,2,1,projection="3d")
+axes2=fig.add_subplot(2,2,2,projection="3d")
+axes3=fig.add_subplot(2,2,3,projection="3d")
+axes4=fig.add_subplot(2,2,4,projection="3d")
+plt.subplots_adjust(wspace=0.2,hspace=0.2)
+
+axes1.plot3D(X,Y,Z) # 三维曲线图
+axes2.plot_surface(XX,YY,ZZ) # 三维曲面图
+axes3.bar3d(X,Y,height,width,depth,Z) # 三维柱状图
+axes4.scatter3D(X,Y,Z) # 三维散点图
+
+plt.show()
+
+#------------------------------------------------------------#
+
+# 3 高级绘图种类：seaborn模块的部分绘图知识
+
+"""
+seaborn是对matplotlib的进一步封装，除了兼容matplotlib大部分功能外，还含有更为高级的绘图技巧
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+X=np.random.binomial(5,0.6,size=10000).reshape(10,1000)
+Y=np.random.normal(0,1,size=10000).reshape(10,1000)
+
+sns.set_style("whitegrid") # 图表风格，默认为darkgrid（同matplotlib），此外有whitegrid, dark, white, ticks
+sns.set_context("paper") # 绘图比例，默认为notebook，还有paper, talk, poster
+pal1=sns.color_palette("hls",8) 
+pal2=sns.color_palette("Paired",8)
+"""
+颜色主题,pal用于保存预设的颜色主题, "hls"(分类色), "Paired"(对比色), n表示颜色种类
+"""
+
+# 直方图与核密度图
+sns.displot(X[0], kde=True, rug=True, palette=pal1) # displot是一个独立的画布，只能以单图存在
+"""
+kde(是否添加密度曲线),rug(是否在底部添加观测细条),palette（颜色主题）
+"""
+sns.kdeplot(X[0], shade=True, palette=pal2) # kernel(核函数类型),shade(是否填充曲线下方)
+plt.show()
+
+# 箱型图与小提琴图
+fig,axes=plt.subplots(1,2)
+sns.boxplot(X[0], Y[0], ax=axes[0])
+sns.violinplot(X[0], Y[0], ax=axes[1])
+plt.show()
+
+# 拟合散点图与联合分布图
+sns.jointplot(Y[0], Y[1]) # jointplot是一个独立的画布，只能以单图存在
+fig=plt.figure()
+sns.regplot(Y[0], Y[1])
+plt.show()
+
+# 散点矩阵图与相关系数热力图
+data=pd.DataFrame(
+    {"num1":np.random.normal(0,1,size=1000),
+    "num2":np.random.normal(1,2,size=1000),
+    "num3":np.random.exponential(0.2,size=1000),
+    "num4":np.random.poisson(5,size=1000)
+    },
+    columns=["num1","num2","num3","num4"])
+sns.pairplot(data) # 只用于对数据集绘图
+
+fig=plt.figure()
+cor=data.corr() # 相关系数矩阵
+sns.heatmap(cor)
+plt.show()
